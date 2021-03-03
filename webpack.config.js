3ff44c21f3a1494b68d3045607b41cd2d5931df7
atom.hkt <thombeck@gmail.com>
2021-03-03 18:34:59 +0700

    update leaflet css

--- webpack.config.js
+++ webpack.config.js
@@ -8,14 +8,6 @@ module.exports = {
   },
   module: {
     rules: [
-			// {
-      //   test: /\.(png|jpg|svg|gif)$/i,
-      //   use: [
-      //     {
-      //       loader: 'file-loader',
-      //     },
-      //   ],
-      // },
       {
         test: /\.js$/,
         exclude: /node_modules/,
